72ac1ac6f169c70b7dcbc35dbf1c3addbb8095af
sweet-peach <127978648+sweet-peach@users.noreply.github.com>
2024-10-27 22:17:12 +0100

    Created a barebones backend server

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import mongoose from "mongoose";
+import 'dotenv/config'
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const MONGODB_URL = process.env.MONGODB_URL;
+
+const start = async () => {
+    try {
+        await mongoose.connect(MONGODB_URL);
+
+        app.listen(PORT, ()=>{
+            console.log(`Server started listening port ${PORT}`)
+        })
+    } catch (e) {
+        console.log(e);
+        process.exit(1);
+    }
+}
+
+start();
