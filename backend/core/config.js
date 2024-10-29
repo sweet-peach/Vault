802b8afb1839ed928085a097def41dc73fc94c9a
sweet-peach <127978648+sweet-peach@users.noreply.github.com>
2024-10-29 04:05:43 +0100

    Config update

--- backend/core/config.js
+++ backend/core/config.js
@@ -1,7 +1,7 @@
 const config = {
     avatar_maximum_size: 1024*1024*5,
-    files_directory: "C:\\Users\\ajsh0\\Desktop\\Vault\\backend\\files",
-    avatars_directory: "C:\\Users\\ajsh0\\Desktop\\Vault\\backend\\avas",
+    files_directory: "", // Sets the directory for storing files. Defaults to the server root. If empty, a "files" directory will be created at boot to store all data.
+    avatars_directory: "", // Sets the directory for storing avatars. Defaults to the server root. If empty, an "avatars" directory will be created at boot to store all data.
     jwt_expiration_in_hours: 24
 }
 
